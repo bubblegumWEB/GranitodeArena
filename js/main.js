843690507e4baba7bf32ac473e89ced025adb61e
bubblegumWEB <gregorickliriano@bubblegum.do>
2022-10-05 02:38:20 -0400

    devices scroll video

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ menuresponsive.addEventListener("click", () => {
    menusticky.classList.toggle("activo");
 })
 
+
+
 // VIDEO
 
 const registerVideo = (bound, video) => {
@@ -15,9 +17,13 @@ const registerVideo = (bound, video) => {
 		if(video.duration) {
 			const distanceFromTop = window.scrollY + bound.getBoundingClientRect().top - 100;
 			const rawPercentScrolled = (window.scrollY - distanceFromTop) / (bound.scrollHeight - window.innerHeight);
-			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 3);
+			const percentScrolled = Math.min(Math.max(rawPercentScrolled, 0), 5);
+			if (window.innerWidth > 1900) {
+        video.currentTime = video.duration * percentScrolled / 2.5;
+     } else {
+      video.currentTime = video.duration * percentScrolled / 1.25;
+     }
 			
-			video.currentTime = video.duration * percentScrolled / 3;
 		}
 		requestAnimationFrame(scrollVideo);
 	}
